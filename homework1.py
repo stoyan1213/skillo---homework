6410ca448d4ccbdf24361d2addd497ae7fac9d48
stoyan1213 <stoyangeorgiev1724@gmail.com>
2024-07-21 17:50:03 +0300

    Add files via upload

---
+++ homework1.py
@@ -0,0 +1,6 @@
+name = "Stoyan"
+age = 33
+fcolor = "red"
+
+
+print(f"My name is {name}, I am {age} years old, and my favorite color is {fcolor}.")
---
+++ homework3.py
@@ -0,0 +1,8 @@
+fahrenheit = float(input())
+celsium = (fahrenheit - 32) * 5 / 9
+
+print(celsium)
+
+
+
+
